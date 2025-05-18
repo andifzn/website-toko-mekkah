128c3c5422c388c9a81da2a74a2b89d0e42fa179
andifzn <andifauzan586@gmail.com>
2025-05-18 08:38:30 +0800

    ya

--- js-library/gsap.js
+++ js-library/gsap.js
@@ -1,21 +1,19 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.registerPlugin(TextPlugin)
+    gsap.registerPlugin(TextPlugin);
 
-    gsap.from('.navbar', {
-        y : -150
+    gsap.from(".navbar", {
+        y: -150,
     });
-    gsap.to('.navbar', {
-        duration:2,
+    gsap.to(".navbar", {
+        duration: 2,
         ease: "elastic.out(1,0.3)",
-        y: 0
-        });
+        y: 0,
+    });
 
-
-    gsap.from('.tagline', {
-        duration:2,
-        opacity:0,
+    gsap.from(".hero-content", {
+        duration: 2,
+        opacity: 0,
         delay: 2,
         ease: "slow(0.7,0.7,false)",
     });
-
 });
