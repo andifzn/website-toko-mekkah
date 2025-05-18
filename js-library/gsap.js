88095284de13238ef1df0a37f818912e9c8f4faf
andifzn <andifauzan586@gmail.com>
2025-05-18 22:03:52 +0800

    gatau

--- js-library/gsap.js
+++ js-library/gsap.js
@@ -11,9 +11,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
     });
 
     gsap.from(".hero-content", {
-        duration: 1.1,
+        duration: 1,
         opacity: 0,
-        delay: 2,
+        delay: 1,
         ease: "slow(0.7,0.7,false)",
     });
 });
